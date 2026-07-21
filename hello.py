0279c0d79ec01ebe349f40b1c632a5d7f5cd71cf
gabrielifeadike2 <gabrielifeadike2@gmail.com>
2026-07-21 08:46:20 +0100

    Compound Interest Calculator

--- hello.py
+++ hello.py
@@ -1,12 +1,31 @@
-unit = input("Is this temperature in Celsius or Fahrenheit: (C or F): ")
-temp = float(input("Enter the temperature: " ))
+principle = 0
+rate = 0
+time = 0
 
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f" The temperature in Fahrenheit is: {temp}°F")
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"The temperature in Celsius is: {temp}°C")
-    
-else:
-    print(f"{unit} is an invalid unit of measurement")
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle < 0:
+        print("Principle can't be less than zero")
+    else:
+        break
+
+while True:
+    rate = float(input("Enter the interest rate: "))
+    if rate <  0:
+        print("Interest rate can't be less than zero")
+    else:
+        break
+
+while True:
+    time = int(input("Enter the time in years: "))
+    if time < 0:
+        print("Time can't be less than zero")
+    else:
+        break
+
+total = principle * pow((1 + rate / 100), time)
+print(f"Balance after {time} year/s: £{total:.2f}")
+          
+
+          
+
